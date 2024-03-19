ffe6ea1678703c9bb6f20f3c681a58dda3764ec1
Alexandros Fourtounis <theadventurer2003@gmail.com>
2024-03-19 12:49:06 +0200

    finally read is fixed and working as expected

--- KV_Store/kv_store.c
+++ KV_Store/kv_store.c
@@ -36,74 +36,51 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        int key_len = 0;
+        int out_len = 0;
         FILE *db = fopen("db.txt", "a");
         if (db == NULL)
         {
             printf("Error opening file\n");
             return 1;
         }
-        char key_str[20];
-        sprintf(key_str, "%d", key);
+        char kv_str[40];
+        sprintf(kv_str, "%d,%d", key1, value);
         if (EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
         {
             printf("Error initializing encryption\n");
             return 1;
         }
-        int key_length = strlen(key_str) + 1;
-        unsigned char *key_out = (unsigned char *)malloc(key_length + 16);
-        if (EVP_EncryptUpdate(ctx, key_out, &key_len, (unsigned char *)key_str, key_length) != 1)
+        int kv_length = strlen(kv_str) + 1;
+        unsigned char *kv_out = (unsigned char *)malloc(kv_length + 16);
+        if (EVP_EncryptUpdate(ctx, kv_out, &out_len, (unsigned char *)kv_str, kv_length) != 1)
         {
-            printf("Error encrypting key\n");
+            printf("Error encrypting key-value pair\n");
             return 1;
         }
-        int key_out_len = key_len;
-        if (EVP_EncryptFinal_ex(ctx, key_out + key_len, &key_len) != 1)
-        {
-            printf("Error finalizing encryption\n");
-            return 1;
-        }
-        key_out_len += key_len;
-        EVP_CIPHER_CTX_cleanup(ctx);
-
-        int value_len = 0;
-        char value_str[20];
-        sprintf(value_str, "%d", value);
-        int value_length = strlen(value_str) + 1;
-        printf("length of value_str = %d\n", value_length);
-        unsigned char *value_out = (unsigned char *)malloc(value_length + 16);
-        if (EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
-        {
-            printf("Error initializing encryption\n");
-            return 1;
-        }
-        if (EVP_EncryptUpdate(ctx, value_out, &value_len, (unsigned char *)value_str, value_length) != 1)
-        {
-            printf("Error encrypting key\n");
-            return 1;
-        }
-        int value_out_len = value_len;
-        if (EVP_EncryptFinal_ex(ctx, value_out + value_len, &value_len) != 1)
+        int kv_out_len = out_len;
+        if (EVP_EncryptFinal_ex(ctx, kv_out + out_len, &out_len) != 1)
         {
             printf("Error finalizing encryption\n");
             return 1;
         }
-        value_out_len += value_len;
+        kv_out_len += out_len;
         EVP_CIPHER_CTX_cleanup(ctx);
+        char *hex_out = (char *)malloc(kv_out_len * 2 + 1);
+        for (int i = 0; i < kv_out_len; i++)
+        {
+            sprintf(hex_out + i * 2, "%02x", kv_out[i]);
+        }
+        hex_out[kv_out_len * 2] = '\0';
 
-        fwrite(key_out, 1, key_out_len, db);
-        fprintf(db, ",");
-        fwrite(value_out, 1, value_out_len, db);
-        fprintf(db, "\n");
+        fprintf(db, "%s\n", hex_out);
         fclose(db);
         EVP_CIPHER_CTX_free(ctx);
-        printf("DEBUG: key and value was encrypted and appended\n");
+        printf("DEBUG: key-value pair was encrypted and appended\n");
     }
     else if (strcmp(operation, "read") == 0)
     {
         key1 = atoi(argv[4]);
-        char key1_str[20];
-        sprintf(key1_str, "%d", value);
+
 
         EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
 
@@ -118,53 +95,64 @@ int main(int argc, char *argv[])
         {
             line[strcspn(line, "\n")] = '\0';
             printf("DEBUG line: %s\n", line);
-            char *key_str = strtok(line, ",");
+            // Convert line from hexadecimal to binary
+            int line_len = strlen(line);
+            unsigned char *line_bin = (unsigned char *)malloc(line_len / 2);
+            for (int i = 0; i < line_len; i += 2)
+            {
+                sscanf(line + i, "%2hhx", &line_bin[i / 2]);
+            }
+            line_len /= 2;
+            int out_len, final_len;
+            final_len = 0;
+            unsigned char *line_out = malloc(line_len + 16);
+            printf("DEBUG line in bin: %s\n", line_bin);
+
+            if (EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
+            {
+                printf("Error initializing decryption context\n");
+                return 1;
+            }
+            if (EVP_DecryptUpdate(ctx, line_out, &out_len, line_bin, line_len) != 1)
+            {
+                printf("Error decrypting line\n");
+                return 1;
+            }
+            out_len += final_len;
+            if (EVP_DecryptFinal_ex(ctx, line_out + out_len, &final_len) != 1)
+            {
+                printf("Error finalizing decryption\n");
+                ERR_print_errors_fp(stderr);
+                return 1;
+            }
+            out_len += final_len;
+            line_out[out_len] = '\0';
+            EVP_CIPHER_CTX_cleanup(ctx);
+            printf("Decrypted line: %s\n", line_out);
+
+            char *key_str = strtok((char *)line_out, ",");
             char *value_str = strtok(NULL, ",");
-            int key_len, value_len, out_len, final_len;
-            unsigned char *key_out = malloc(EVP_MAX_BLOCK_LENGTH);
-            unsigned char *value_out = (unsigned char *)malloc(strlen(value_str) + 1);
-            // unsigned char key_out[EVP_MAX_BLOCK_LENGTH+1024];
-            key_len = 0;
             if (!key_str || !value_str)
             {
                 printf("Error parsing line\n");
                 return 1;
             }
 
-            printf("DEBUG: key_str = %s, value_str = %s\n", key_str, value_str);
-
-            if (EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
+            printf("Key: %s, Value: %s\n", key_str, value_str);
+            
+          
+            printf("DEBUG: key_str: %s as an int %d\n", key_str, atoi(key_str));
+            if(atoi(key_str) == key1)
             {
-                printf("Error initializing decryption context\n");
-                return 1;
-
+                printf("SUCCESS: %s\n", value_str);
+                free(line_bin);
+                free(line_out);
+                return 0;
             }
-            //EVP_CIPHER_CTX_set_padding(ctx, 0);
-            if (EVP_DecryptUpdate(ctx, key_out, &key_len, (unsigned char *)key_str, strlen(key_str)) != 1)
-            {
-                printf("Error decrypting key\n");
-                return 1;
-            }
-            out_len = key_len;
-            if (EVP_DecryptFinal_ex(ctx, key_out + key_len, &final_len) != 1)
-            {
-                printf("Error finalizing key decryption\n");
-                ERR_print_errors_fp(stderr);
-                return 1;
-            }
-            out_len += final_len;
-            key_out[out_len] = '\0';
-            EVP_CIPHER_CTX_cleanup(ctx);
-            printf("Key: %s, Value:\n", key_out);
-
-            if (strcmp((char *)key_out, key_str) == 0)
-            {
-                printf("SUCCESS Key: %s, Value: \n", (char *)key_str);
-            }
-
-            EVP_CIPHER_CTX_free(ctx);
+            free(line_bin);
+            free(line_out);
         }
-
+        free(ctx);
         fclose(db);
 
         printf("DEBUG: read was entered\n");
